ea4c0dfb0eec19ee6b709c86f6731debc3852e72
Den Push <pushkerdenys@gmail.com>
2022-05-04 21:03:27 +0300

    updating app with reselect? replaced cart selectors with memorized selectors

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -9,8 +9,24 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
             ...cartItem,
             quantity: cartItem.quantity + 1,
           }
-        : { cartItem }
+        : cartItem
     );
   }
   return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
 };
+
+// export const addItemToCart = (cartItems, cartItemToAdd) => {
+//   const existingCartItem = cartItems.find(
+//     (cartItem) => cartItem.id === cartItemToAdd.id
+//   );
+
+//   if (existingCartItem) {
+//     return cartItems.map((cartItem) =>
+//       cartItem.id === cartItemToAdd.id
+//         ? { ...cartItem, quantity: cartItem.quantity + 1 }
+//         : cartItem
+//     );
+//   }
+
+//   return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+// };
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,4 @@
-import { applyMiddleware, configureStore, createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
 import logger from "redux-logger";
 
 import rootReducer from "./root-reducer";
