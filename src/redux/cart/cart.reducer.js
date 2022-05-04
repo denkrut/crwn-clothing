0833d397577bc6f4a6e16ed357548239e831c419
Den Push <pushkerdenys@gmail.com>
2022-05-04 11:44:28 +0300

    updated cart utils to group items in cart reducer

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,5 @@
-import { CartActionTypes } from "./cart.types";
 import { addItemToCart } from "./cart.utils";
+import { CartActionTypes } from "./cart.types";
 
 const INITIAL_STATE = {
   hidden: true,
